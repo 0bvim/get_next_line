0d9f97b235d6b0a396c85ef50130d49640f23bb5
vde-frei <vde-frei@student.42sp.org.br>
2023-08-21 13:34:32 -0300

    fix: need to update some files yet  Changes to be committed: 	modified:   Makefile 	deleted:    get_next_line 	modified:   get_next_line.c 	modified:   get_next_line.h 	deleted:    get_next_line_bonus.c 	modified:   get_next_line_bonus.h 	deleted:    get_next_line_utils_bonus.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,15 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 14:32:39 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/08/21 12:52:12 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/08/21 13:24:19 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
 	static t_file_info	file;
-
 	if (file.pos >= file.read || file.pos == 0)
 	{
 		file.pos = 0;
@@ -79,24 +77,50 @@ char	*ft_build_line(t_file_info *set)
 	line[count] = '\0';
 	return (line);
 }
-
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*retrn;
-
-	fd = open(argv[1], O_RDONLY);
-	(void)argc;
-	retrn = "";
-	while (retrn != NULL)
-	{
-		retrn = get_next_line(fd);
-		printf("%s", retrn);
-		free(retrn);
-	}
-	close (fd);
-	return (0);
-}
+//int	main(int argc, char **argv)
+//{
+//	int		fd;
+//	int		fd1;
+//	int		fd2;
+//	int		fd3;
+//	char	*retrn;
+//
+//	fd = open(argv[1], O_RDONLY);
+//	fd1 = open(argv[2], O_RDONLY);
+//	fd2 = open(argv[3], O_RDONLY);
+//	fd3 = open(argv[4], O_RDONLY);
+//	(void)argc;
+//	retrn = "";
+//	while (retrn != NULL)
+//	{
+//		retrn = get_next_line(fd);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd1);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd2);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd3);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd1);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd2);
+//		printf("%s", retrn);
+//		free(retrn);
+//		retrn = get_next_line(fd3);
+//		printf("%s", retrn);
+//		free(retrn);
+//	}
+//	close (fd);
+//	return (0);
+//}
 //#include <string.h>
 //int	main(int argc, char **argv)
 //{
